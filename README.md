a2e534885b88fbf3e0762bd5371c27274e87fd25
aslon1213 <hamidovaslon1@gmail.com>
2023-04-15 19:12:41 +0500

    first commit

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # My-Leetcode-Solutions
 
-| Problem | Language | Link |
-| ------- | -------- | ---- |
-| 1       | 2        | 3    |
-| 4       | 5        | 6    |
-
-fas
+| Problem | Language | Link                                                                                   |
+| ------- | -------- | -------------------------------------------------------------------------------------- |
+| 1       | 2        | 3                                                                                      |
+| 4       | 5        | 6                                                                                      |
+| 2218    | go       | https://github.com/aslon1213/My-Leetcode-Solutions/src/solution 2218%!(EXTRA int=2218) |
---
+++ utils/main.go
@@ -0,0 +1,46 @@
+// a program to add a new solution to Readme.md
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+)
+
+func main() {
+	//get the current directory
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// get the solution number
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter solution number: ")
+
+	text, _, _ := reader.ReadLine()
+	solutionNumber, err := strconv.Atoi(string(text))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// construct a link
+	link := fmt.Sprintf("https://github.com/aslon1213/My-Leetcode-Solutions/src/solution %d", solutionNumber, solutionNumber)
+	// construct a new line
+	newLine := fmt.Sprintf("| %d | %s |%s |", solutionNumber, "go", link)
+	// write the new line to the readme file
+	f, err := os.OpenFile(filepath.Join(dir, "Readme.md"), os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+	if _, err = f.WriteString(newLine); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+}
